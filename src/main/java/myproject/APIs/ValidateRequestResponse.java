a33d1ecb6b286f8b42e9779c72ac2861af819371
Anish Mathew <mathewani@gmail.com>
2026-01-13 20:11:21 -0500

    new changes

--- src/main/java/myproject/APIs/ValidateRequestResponse.java
+++ src/main/java/myproject/APIs/ValidateRequestResponse.java
@@ -1,11 +1,6 @@
 package myproject.APIs;
-import io.restassured.RestAssured;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class ValidateRequestResponse {
-
-    @BeforeMethod
     public void getRequest(){
 
     }
